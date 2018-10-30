a763d706e8fc80b4409c581050111155ceed14ca
Augusto Mesquita <augustomesquitasrs@gmail.com>
2018-10-30 18:18:32 -0300

    Apidoc added to project.

--- src/app.ts
+++ src/app.ts
@@ -25,14 +25,17 @@ class App {
         // enable cors on application
         this.app.use(cors(this.corsOptions))
 
+        // url to access api documentation
+        this.app.use('/apidoc', express.static('apidoc'));
+
         // support application/json type post data
         this.app.use(bodyParser.json());
 
         // support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
 
-        this.app.use('/', main);
-        this.app.use('/person', personController);
+        this.app.use('/api/v1/', main);
+        this.app.use('/api/v1/person', personController);
     }
 
 }
--- src/controller/personController.ts
+++ src/controller/personController.ts
@@ -11,10 +11,23 @@ class PersonController {
     }
 
     private config(): void {
+
         this.router.get('/', (req: Request, res: Response) => {
             res.send('Hello, World!');
         });
 
+
+        /**
+         * 
+         * @api {GET} /person getById
+         * @apiName nodeapi
+         * @@apiParam  {String} id Id da person
+         * @apiGroup person
+         * @apiVersion  0.0.1
+         * 
+         * @apiSuccess (200) {type} id descrição
+         * 
+         */
         this.router.get('/:id', (req: Request, res: Response) => {
             let { id }  = req.params;
             res.send(`Hello, ${id}`);
