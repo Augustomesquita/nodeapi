585c05de945202321ac8037574df034875b2d460
Augusto Mesquita <augustomesquitasrs@gmail.com>
2018-11-01 10:29:34 -0300

    Adicionado exemplo de pedido na documentação da API.

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -24,6 +24,7 @@ class UserController {
          * @apiGroup user
          * @apiVersion  0.0.1
          * @@apiPermission all
+         * @apiSampleRequest /api/v1/user/:id
          * 
          * @apiParam  {string} id Id do usuário
          * 
