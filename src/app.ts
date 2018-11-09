185bb5068db7475df29a68d272c1fa025e2a4b82
Augusto Mesquita <augustomesquitasrs@gmail.com>
2018-11-09 18:14:03 -0200

    Adicionado rotina para ativar um alarme todos os dias às 00.00.

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ class App {
     constructor() {
         this.app = express();
         this.config();
+        this.startBackgroundServices();
     }
 
     private config(): void {
@@ -51,6 +52,40 @@ class App {
     private routes() {
         this.app.use('/api/v1/', mainRoute);
         this.app.use('/api/v1/users', userRoute);
+    }
+
+    private startBackgroundServices() {
+        setTimeout(() => this.setAlarmClock(true), 0);
+    }
+
+
+    private setAlarmClock(firstExecution: boolean) {
+
+        let now = new Date();
+        let timeUntilNextCall: number;
+
+        const hourOfAlarm = 0;
+        const minuteOfAlarm = 0;
+
+        if (!firstExecution) {
+            timeUntilNextCall = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hourOfAlarm, minuteOfAlarm, 0, 0).getTime() - now.getTime();
+            if (timeUntilNextCall <= 0) {
+                console.log("!!!DESPERTADOR ATIVADO AQUI!!!");
+                let tommorrow: Date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hourOfAlarm, minuteOfAlarm, 0, 0);
+                tommorrow.setDate(tommorrow.getDate() + 1) // Adiciona um dia.
+                timeUntilNextCall = tommorrow.getTime() - now.getTime(); // Seta horário da próxima chamada da função.
+            }
+        } else {
+            timeUntilNextCall = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hourOfAlarm, minuteOfAlarm, 0, 0).getTime() - now.getTime();
+            if (timeUntilNextCall <= 0) {
+                console.log("***ALARME NÃO SERÁ DISPARADO POIS JÁ PASSOU DA HORA E O SERVIDOR ACABOU DE SUBIR***");
+                let tommorrow: Date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hourOfAlarm, minuteOfAlarm, 0, 0);
+                tommorrow.setDate(tommorrow.getDate() + 1); // Adiciona um dia
+                timeUntilNextCall = tommorrow.getTime() - now.getTime();
+            }
+        }
+
+        setTimeout(() => this.setAlarmClock(false), timeUntilNextCall);
     }
 
 }
