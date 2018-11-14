cfbd138794c92164acd4dde478009d8d09c19d00
Augusto Mesquita <augustomesquitasrs@gmail.com>
2018-11-14 16:13:02 -0200

    Adicionado angular material e configurado cors para aceitar qualquer origem.

--- api/src/app.ts
+++ api/src/app.ts
@@ -20,7 +20,7 @@ export class App {
 
     // CORS Settings.
     private corsOptions = {
-        origin: 'http://localhost:4200',
+        origin: '*',
         optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
     };
 
